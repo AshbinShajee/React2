cb00aa3a12fc379a85ffed0530228766632aa3ca
AshbinShajee <134415836+AshbinShajee@users.noreply.github.com>
2024-02-02 11:36:18 +0530

    inside the app

---
+++ App.js
@@ -0,0 +1,44 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+      <h3>Epic Games: An American Video Game And Software Developer And Publisher Based In Cary, North California</h3>
+      <br />
+      <p>Create, play, and battle with friends for free in Fortnite. Be the last player standing in Battle Royale and Zero Build, experience a concert or live event, or discover over a million creator-made games, including racing, parkour, zombie survival, and more. Each Fortnite island has an individual age rating so you can find the one that's right for you and your friends. Find it all in Fortnite ... Drop In.</p>
+      <br />
+      <button style={{ backgroundColor: 'ghostwhite', color: 'black' }}>Visit Website</button>
+      <br />
+      <p>Explore large, destructible environments where no two games are ever the same.</p>
+      <br />
+      <p>Team up with friends by sprinting, climbing and smashing your way to earn your Victory Royale</p>
+      <br />
+      <p>Discover even more ways to play across thousands of creator-made game genres</p>
+      <br />
+      <h3>Our Contribution</h3>
+      <br />
+      <p> Our core offering extends beyond mere profit generation; we emphasize the growth and active involvement of our user community. Collaborating with us represents an investment, rather than a mere expenditure. Our dedication to providing distinctive digital interactions guarantees unparalleled benefits for our clientele.</p>
+      <br />
+      <h3>5M</h3>
+      <br />
+      <p>Daily user engagements</p>
+      <br />
+      <h3>$500K</h3>
+      <br />
+      <p>Revenue surge for a platform</p>
+      <h3>10X</h3>
+      <br />
+      <p> ROAS on all our marketing campaigns</p>
+      <h3>Interested in delving deeper into the project?</h3>
+      <br />
+      <p>If you'd like to explore further details about our initiatives or any of our affiliated brands, don't hesitate to connect. You can reach out to us via email at hello@abc.com or give us a call at +91 480 20802730.
+        Ring us on Skype
+        Contact Us</p>
+      <button style={{ backgroundColor: 'black', color: 'white' }}>Ring us on Skype</button>
+      <br />
+      <button style={{ backgroundColor: 'white', color: 'black' }}>Contact us</button>
+    </div>
+  );
+};
+
+export default App;
